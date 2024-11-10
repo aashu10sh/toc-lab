de5e1dd3e6e502b241822234119bb84c5bc80fb8
Aashutosh Pudasaini <nepalidude3@gmail.com>
2024-11-10 13:45:59 +0545

    add: states logic in lab2

--- lab_2.c
+++ lab_2.c
@@ -2,21 +2,38 @@
 #include<string.h>
 #include<stdlib.h>
  // to check if string ends with an 'ab'
+enum States{
+  INITIAL,
+  FIRST,
+  SECOND
+ };
+
+ typedef enum States State;
+
+
 
 int main(void) {
   char * string = "this-is-so-cool-bro-ab";
   int string_len = strlen(string);
+  State current_state = INITIAL;
   
   if (string_len < 2) {
     printf("Need String of Length at least 2");
     return EXIT_FAILURE;
   }
+  if (string[string_len - 2] == 'a'){
+    current_state = FIRST;
+    if (string[string_len - 1] == 'b'){
+      current_state = SECOND;
+    }  
+  }
 
-  if (string[string_len - 2] == 'a' && string[string_len - 1] == 'b') {
-    printf("String Passes!\n");
+  if (current_state == SECOND){
+    printf("Condition Satisfied!\n");
     return EXIT_SUCCESS;
   }
 
-  printf("String Fails!\n");
+  printf("Condition Failed!\n"); 
+
   return EXIT_FAILURE;
 }
