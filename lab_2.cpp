af9ea80cf7471df37db4f66783b0d002ee639e86
Aashutosh Pudasaini <nepalidude3@gmail.com>
2024-11-10 15:53:53 +0545

    make c -> cpp for lab 2

--- lab_2.cpp
+++ lab_2.cpp
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<iostream>
  // to check if string ends with an 'ab'
 enum States{
   INITIAL,
@@ -10,14 +11,13 @@ enum States{
 
 typedef enum States State;
 
-int main(void) {
-  printf("This code is run by Aashutosh Pudasaini - 1202\n");
-  char * string = "this-is-so-cool-bro-ab";
+bool ends_with_ab(const char * string) {
+
   int string_len = strlen(string);
   State current_state = INITIAL;
   
   if (string_len < 2) {
-    printf("Need String of Length at least 2");
+    printf("Need String of Length at least 2\n");
     return EXIT_FAILURE;
   }
   if (string[string_len - 2] == 'a'){
@@ -29,10 +29,28 @@ int main(void) {
 
   if (current_state == SECOND){
     printf("Condition Satisfied!\n");
-    return EXIT_SUCCESS;
+    return true;
   }
 
   printf("Condition Failed!\n"); 
+  return false;
+}
 
-  return EXIT_FAILURE;
+int main(void){
+
+  printf("This code is run by Aashutosh Pudasaini - 1202\n");
+
+    std::string testCases[] = {
+    "0",
+    "001a",
+    "00000ab",
+    "1111bba",
+    "0101ab",
+    "001ab10"
+  };
+
+  for (const std::string & testCase: testCases) {
+    printf("%s: ", testCase.c_str());
+    ends_with_ab(testCase.c_str());
+  }
 }
